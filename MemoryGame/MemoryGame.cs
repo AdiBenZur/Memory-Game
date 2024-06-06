6c15ab5d4f07ece9485119f45ddb5872d9901cac
AdiBenZur <adi.benzur@gmail.com>
2024-06-06 09:46:56 +0300

    Add project files.

---
+++ MemoryGame/MemoryGame.cs
@@ -0,0 +1,306 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace MemoryGame
+{
+    public struct LocationOfCell
+    {
+        private int m_Row;
+        private int m_Col;
+
+        public LocationOfCell(int i_Row, int i_Col)
+        {
+            m_Row = i_Row;
+            m_Col = i_Col;
+        }
+
+        public int Row
+        {
+            get
+            {
+                return m_Row;
+            }
+            set
+            {
+                m_Row = value;
+            }
+        }
+
+        public int Col
+        {
+            get
+            {
+                return m_Col;
+            }
+            set
+            {
+                m_Col = value;
+            }
+        }
+    }
+
+    /// ///////////////////////////////////////////////////////////////////////
+
+    public class Cell
+    {
+        private char m_Value;
+        private LocationOfCell m_CellLocation;
+        private LocationOfCell m_CoupleLocation;
+        private bool v_IsExposed = false;
+        
+        public char Value
+        {
+            get
+            {
+                return m_Value;
+            }
+            set
+            {
+                m_Value = value;
+            }
+        }
+
+        public Cell(int i_Row, int i_Col, char i_Value, LocationOfCell i_CoupleLocation)
+        {
+            m_CellLocation = new LocationOfCell(i_Row, i_Col);
+            m_Value = i_Value;
+            m_CoupleLocation = i_CoupleLocation;
+        }
+
+        public bool IsExposed()
+        {
+            return this.v_IsExposed;
+        }
+    }
+
+    /// ///////////////////////////////////////////////////////////////////////
+    public class User
+    {
+        private string m_Name = null;
+        private int m_NumberOfPoints = 0;
+        private bool v_IsComputer;
+
+
+        public int NumberOfPoints
+        {
+            get
+            {
+                return m_NumberOfPoints;
+            }
+            set
+            {
+                m_NumberOfPoints = value;
+            }
+        }
+        public string Name
+        {
+            get
+            {
+                return m_Name;
+            }
+            set
+            {
+                m_Name = value;
+            }
+        }
+        public bool IsComputer
+        {
+            get
+            {
+                return v_IsComputer;
+            }
+            set
+            {
+                v_IsComputer = value;
+            }
+        }
+
+        public User(string i_Name, bool i_IsComputer)
+        {
+            m_Name =  i_Name;
+            v_IsComputer = i_IsComputer;
+        }
+
+    }
+
+    //////////////////////////////////////////////////////////////////////////
+
+    public class MemoryGameLogic
+    {
+        private uint m_BoardRows = 0;
+        private uint m_BoardCols = 0;
+        private uint m_Turn = 0;
+        private uint m_TakenCells = 0;
+        private Cell[,] m_MatrixBoard = null;
+        private User m_FirstUser;
+        private User m_SecondUser;
+
+        public uint Rows
+        {
+            get
+            {
+                return m_BoardRows;
+            }
+            set
+            {
+                m_BoardRows = value;
+            }
+        }
+
+        public uint Cols
+        {
+            get
+            {
+                return m_BoardCols;
+            }
+            set
+            {
+                m_BoardCols = value;
+            }
+        }
+
+        public uint Turn
+        {
+            get
+            {
+                return m_Turn;
+            }
+            set
+            {
+                m_Turn = value;
+            }
+        }
+
+        public uint TakenCells
+        {
+            get
+            {
+                return m_TakenCells;
+            }
+            set
+            {
+                m_TakenCells = value;
+            }
+        }
+
+        public Cell[,] getMatrixBoard()
+        {
+            return m_MatrixBoard;   
+        }
+
+        public MemoryGameLogic(string i_FirstUserName, string i_SecondUserName, bool i_IsSecondPlayerComputer, uint i_Rows, uint i_Cols)
+        {
+            m_FirstUser = new User(i_FirstUserName, false);
+            m_SecondUser = new User(i_SecondUserName, i_IsSecondPlayerComputer);
+            m_MatrixBoard = new Cell[i_Rows,  i_Cols];
+            m_BoardCols = i_Cols;
+            m_BoardRows = i_Rows;
+        }
+
+        public void InitializeBoard()
+        {
+            char charToInsert = 'A';
+            List<LocationOfCell> alreadyRaffledCells = new List<LocationOfCell>(); // List of full cells
+            Random random = new Random();
+            uint numberOfLetters = m_BoardCols * m_BoardRows / 2;
+
+            for (int i = 0; i < numberOfLetters; i++)
+            {
+                // Raffled two cell's locations
+                int firstCellRow = random.Next(0, (int)m_BoardRows - 1);
+                int firstCellCol = random.Next(0, (int)m_BoardRows - 1);
+
+                // Check if this cell already Raffled
+                while (isCellAlreadyRaffled(alreadyRaffledCells, firstCellRow, firstCellCol))
+                {
+                    firstCellRow = random.Next(0, (int)m_BoardRows - 1);
+                    firstCellCol = random.Next(0, (int)m_BoardRows - 1);
+                }
+
+                alreadyRaffledCells.Add(new LocationOfCell(firstCellRow, firstCellCol));
+
+                int secondCellRow = random.Next(0, (int)m_BoardRows - 1);
+                int secondCellCol = random.Next(0, (int)m_BoardRows - 1);
+
+                while (isCellAlreadyRaffled(alreadyRaffledCells, firstCellRow, firstCellCol))
+                {
+                    firstCellRow = random.Next(0, (int)m_BoardRows - 1);
+                    firstCellCol = random.Next(0, (int)m_BoardRows - 1);
+                }
+
+                alreadyRaffledCells.Add(new LocationOfCell(secondCellRow, secondCellCol));
+
+                // Set the value
+                m_MatrixBoard[firstCellRow, firstCellCol].Value = charToInsert;
+                m_MatrixBoard[secondCellRow, secondCellCol].Value = charToInsert;
+
+                // Move to next char
+                charToInsert += '1';
+            }
+        }
+
+        private bool isCellAlreadyRaffled(List<LocationOfCell> i_RaffledCellsList, int i_Rows, int i_Cols)
+        {
+            bool isInList = true;
+
+            for (int i = 0; i < i_RaffledCellsList.Count; i++)
+            {
+                if (i_RaffledCellsList[i].Row == i_Rows && i_RaffledCellsList[i].Col == i_Cols)
+                {
+                    isInList = false;
+                }
+            }
+
+            return !isInList;
+        }
+
+        public bool IsBoardSizeEven()
+        {
+            return (m_BoardRows * m_BoardCols) % 2 == 0 ;
+        }
+
+        public void CurrentTurn()
+        {
+
+        }
+        public void SwitchToNextPlayer()
+        {
+            if(m_Turn == 0)
+            {
+                m_Turn = 1;
+            }
+            else
+            {
+                m_Turn = 0;
+            }
+        }
+        public string WhichPlayerWon()
+        {
+            if(m_FirstUser.NumberOfPoints > m_SecondUser.NumberOfPoints)
+            {
+                return m_FirstUser.Name + " won with " + m_FirstUser.NumberOfPoints + " points.";
+            }
+            else if(m_FirstUser.NumberOfPoints < m_SecondUser.NumberOfPoints)
+            {
+                return m_SecondUser.Name + " won with " + m_SecondUser.NumberOfPoints + " points.";
+            }
+            else
+            {
+                return "Its a Tie.";
+            }
+        }
+        public bool IsBoardFull()
+        {
+            return m_TakenCells == m_BoardCols * m_BoardRows;
+        }
+
+        public bool IsCellExposed(int i_Row, int i_Col)
+        {
+            return m_MatrixBoard[i_Row, i_Col].IsExposed();
+        }
+    }
+}
---
+++ UserInterface/Userinterface.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MemoryGame;
+using Ex02.ConsoleUtils;
+using System.Diagnostics;
+
+namespace UserInterface
+{
+
+    public enum ePlayersSign
+    {
+        Computer,
+        Human
+    }
+
+    public class MemoryGameUserinterface
+    {
+        private MemoryGameLogic m_MemoryGame;
+        private bool v_IsAgainstComputer;
+        private const string k_QuitSign = "Q";
+        public const uint k_MaxBoardSize = 6;
+        public const uint k_MinBoardSize = 4;
+
+        public void Activate()
+        {
+            startScreenAndScanUserName();
+            chooseIfPlayAgainstUmanOrComputer();
+        }
+
+        private void startScreenAndScanUserName()
+        {
+            Screen.Clear();
+
+            StringBuilder openingMessage = new StringBuilder();
+
+            openingMessage.Append("Wellcome to Memory game!");
+            openingMessage.AppendLine();
+            openingMessage.Append("==============================");
+            openingMessage.AppendLine();
+            openingMessage.Append($"For quit the game you can enter {k_QuitSign} any time:)");
+            openingMessage.AppendLine();
+
+            Console.WriteLine(openingMessage.ToString());
+
+            Console.WriteLine("Enter your name: ");
+            string username = Console.ReadLine();
+            // Set user name in game
+        }
+
+        private void chooseIfPlayAgainstUmanOrComputer()
+        {
+            Console.WriteLine("Against who you want to play? ({0} / {1}):", ePlayersSign.Computer.ToString(), ePlayersSign.Human.ToString());
+            string playerSignStr = Console.ReadLine();
+            ePlayersSign playerSign;
+            while (!(Enum.TryParse<ePlayersSign>(playerSignStr, out playerSign)))
+            {
+                Console.WriteLine("Invalid enemy sign input! Try Again.");
+                Console.Write($"{ePlayersSign.Computer} / {ePlayersSign.Human} : ");
+                playerSignStr = Console.ReadLine();
+            }
+
+            if (playerSign == ePlayersSign.Human)
+            {
+                v_IsAgainstComputer = false;
+                Console.WriteLine("Enter other player name: ");
+                string username = Console.ReadLine();
+                // Set user name in game
+
+            }
+            else
+            {
+                v_IsAgainstComputer = true;
+                // Set user name in game
+            }
+        }
+
+        private void scanBoardSizeFromUser()
+        {
+            string rowsStr;
+            uint rowsSize;
+            string colsStr;
+            uint colsSize;
+            bool isInputValid = true;
+
+            Console.WriteLine("Enter rows size (choose between {0} - {1}): ", k_MinBoardSize, k_MaxBoardSize);
+            do
+            {
+                if (!isInputValid)
+                {
+                    Console.WriteLine("Rows size input is invalid! Try again.");
+                }
+
+                rowsStr = Console.ReadLine();
+                isInputValid = false;
+            }
+            while (!(uint.TryParse(rowsStr, out rowsSize)) || rowsSize < k_MinBoardSize || rowsSize > k_MaxBoardSize);
+
+            m_MemoryGame.Rows = rowsSize;
+            isInputValid = true;
+
+            Console.WriteLine("Enter cols size (choose between {0} - {1}): ", k_MinBoardSize, k_MaxBoardSize);
+            do
+            {
+                if (!isInputValid)
+                {
+                    Console.WriteLine("Cols size input is invalid! Try again.");
+                }
+
+                colsStr = Console.ReadLine();
+                isInputValid = false;
+
+            }
+            while (!(uint.TryParse(colsStr, out colsSize)) || colsSize < k_MinBoardSize || colsSize > k_MaxBoardSize);
+
+            m_MemoryGame.Cols = colsSize;
+        }
+
+        public void PlayTurn()
+        {
+            
+        }
+
+        public void scanCellsLocations()
+        {
+            Console.WriteLine("Enter first cell row and col (e.g. 1B): ");
+            string firstCellLocationInput = Console.ReadLine();
+        }
+
+        private void isCellLocationValid(string i_CellLocation)
+        {
+            bool isCellValid = true;
+            int row;
+            char col;
+
+            do
+            {
+                if (string.IsNullOrEmpty(i_CellLocation) || i_CellLocation.Length != 2)
+                {
+                    Console.WriteLine("Invalid input! It should contain a number and a letter only. Try again.");
+                    isCellValid = false;
+                }
+                else
+                {
+                    // Seperate the input to row and col
+                    if (!TryParseInput(i_CellLocation, out row, out col))
+                    {
+                        Console.WriteLine("Invalid input! It should first contain a number (row) and then a letter (col). Try again.");
+                        isCellValid = false;
+                    }
+                    else
+                    {
+                        // Check if its in range
+                        int rowInBoardMatrix = row - 1;
+                        int colInBoardMatrix = convertColLetterToColInMatrix(col);
+
+                        if (!(rowInBoardMatrix >= 0 && rowInBoardMatrix < m_MemoryGame.Rows && colInBoardMatrix >= 0 && colInBoardMatrix < m_MemoryGame.Cols))
+                        {
+                            Console.WriteLine("Invalid input! The cell is not in the board. Try again.");
+                            isCellValid = false;
+                        }
+                        else
+                        {
+                            // Check if the cell is not exposed
+                            if (m_MemoryGame.IsCellExposed(rowInBoardMatrix, colInBoardMatrix))
+                            {
+                                Console.WriteLine("Invalid input! The cell is already expose. Try again.");
+                                isCellValid = false;
+                            }
+                        }
+                    }
+
+                }
+            }
+            while (!isCellValid);
+
+
+        }
+
+        static bool TryParseInput(string i_CellLocationInput, out int o_Row, out char o_Col)
+        {
+            bool isParsingSucceed = true;
+            o_Row = 0;
+            o_Col = '\0';
+
+            // Extract the numeric part
+            string numberPart = i_CellLocationInput.Substring(0, i_CellLocationInput.Length - 1);
+            // Extract the character part
+            char letterPart = i_CellLocationInput[i_CellLocationInput.Length - 1];
+
+            // Validate the numeric part
+            if (int.TryParse(numberPart, out o_Row) && char.IsLetter(letterPart))
+            {
+                o_Col = letterPart;
+            }
+            else
+            {
+                isParsingSucceed = false;
+            }
+
+            return isParsingSucceed;
+        }
+
+        private int convertColLetterToColInMatrix(char i_CollLetter)
+        {
+            // 'A' is 0, 'B' is 1, 'C' is 2 ...
+            i_CollLetter = char.ToUpper(i_CollLetter);
+            return i_CollLetter - 'A';
+        }
+    }
+
+    
+}
